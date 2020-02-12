89cc3b1e699a4ba4c92744dae0e6ab5940ed57af
Бабак Артем <babak@kinoplan.ru>
2020-02-12 20:09:34 +0300

    Initial commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+    public static final String CSV_DATA_ROOT = "csv_data_root";
+    public static final String XML_DATA_ROOT = "xml_data_root";
+
+    public static final String JDBC_URL = "jdbc_url";
+    public static final String DB_USER = "db_user";
+    public static final String DB_USER_PASSWORD = "db_user_password";
+    public static final String DRIVER_CLASS = "driver_class";
+
+
+    public static final String DATE_FORMAT = "date_format";
+    public static final String DELIMITER_CSV = "delimiter_csv";
+}
