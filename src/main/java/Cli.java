7d8fd2a5696c61c0a97895f25dcdc619f39d5f66
Бабак Артем <babak@kinoplan.ru>
2020-02-15 01:49:47 +0300

    print version of opencv

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -1,5 +1,10 @@
+package main.java;
+
+import org.opencv.core.Core;
+
 public class Cli {
+    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(Core.VERSION);
     }
 }
