8f0117ba3208b14d3fdf693fbf4feeb35055ae74
Бабак Артем <babak@kinoplan.ru>
2020-03-14 00:26:10 +0300

    next

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 package main.java;
 
+import main.java.ImageAPI.ChannelsEnum;
+import main.java.ImageAPI.ImageAPI;
 import org.opencv.core.Core;
 
 public class Main {
-    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
-    public static void main(String[] args) {
+//    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+    public static void main(String[] args) throws Exception {
 //        System.out.println(Core.NATIVE_LIBRARY_NAME);
-        System.out.println(Core.VERSION);
+//        System.out.println(Core.VERSION);
+        ImageAPI instanceImageAPI  = new ImageAPI();
+        instanceImageAPI.loadImage(
+                "color.jpg",
+//                "example.jpg",
+                ChannelsEnum.Color
+        );
+        instanceImageAPI.showImage();
     }
 }
--- src/test/java/InitializeOpenCV.java
+++ src/test/java/InitializeOpenCV.java
@@ -17,8 +17,8 @@ public class InitializeOpenCV {
 //                "example.jpg",
                 ChannelsEnum.Color
         );
-//        instanceImageAPI.showImage();
-        instanceImageAPI.saveImage("color.jpg");
+        instanceImageAPI.showImage();
+//        instanceImageAPI.saveImage("color.jpg");
     }
 
     @Test
